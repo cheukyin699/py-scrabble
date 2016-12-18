b51ef62e1ba2939c7248a21cfccf1ad50e5c6e9d
Cheuk Yin Ng <cheukyin699@yahoo.com>
2016-12-17 22:15:52 -0800

    [resman] Loads board tiles into memory

--- resman.py
+++ resman.py
@@ -3,7 +3,8 @@ import colors
 import word_helpers as wh
 
 Scrabble_Number_Font = "Futura9"
-Scrabble_Font = "Futura40"
+Scrabble_Tile_Font = "Futura40"
+Scrabble_Board_Font = "Futura20"
 Tile_Size = (50, 50)
 '''
 Helps with loading the different fonts and their corresponding sizes.
@@ -14,7 +15,7 @@ Format:
 '''
 Fonts_Config = [
     # Used for scrabble tiles
-    ('Futura',      'FuturaExtended.ttf',       [9, 40]),
+    ('Futura',      'FuturaExtended.ttf',       [9, 20, 40]),
     # Used for everything else
     ('OpenSans',    'OpenSans-Regular.ttf',     [50])
 ]
@@ -23,15 +24,16 @@ Helps with loading the different types of board tiles (i.e. where they are on
 the tile map).
 Format:
     (tile_shortname,
+     should_render_shortname?,
      (x, y, w, h))
 '''
 Board_Tiles = [
-    ('NA', ( 50,  0, 50, 50)),     # Normal tile
-    ('MD', (100,  0, 50, 50)),     # That tile in the middle
-    ('DL', (150,  0, 50, 50)),     # Double letter
-    ('DW', (200,  0, 50, 50)),     # Double word
-    ('TL', (250,  0, 50, 50)),     # Triple letter
-    ('TW', (300,  0, 50, 50))      # Triple word
+    ('NA', False, ( 50,  0, 50, 50)),     # Normal tile
+    ('MD', False, (100,  0, 50, 50)),     # That tile in the middle
+    ('DL',  True, (150,  0, 50, 50)),     # Double letter
+    ('DW',  True, (200,  0, 50, 50)),     # Double word
+    ('TL',  True, (250,  0, 50, 50)),     # Triple letter
+    ('TW',  True, (300,  0, 50, 50))      # Triple word
 ]
 
 
@@ -43,6 +45,7 @@ class ResourceManager:
     '''
     def __init__(self):
         self.fonts = {}
+        self.tiles = {}
 
         self.init_tiles("res/imgs/tile_resources.png")
 
@@ -66,8 +69,10 @@ class ResourceManager:
         self.init_board_tiles()
 
     def init_letter_tiles(self):
+        '''
+        Loads all the playable tiles into memory.
+        '''
         # Initialize tiles by creating them on the fly
-        self.tiles = {}
         for letter in range(ord('A'), ord('Z') + 1):
             # First tile, 50x50
             t = self.tilesMap.subsurface(
@@ -75,7 +80,7 @@ class ResourceManager:
                      Tile_Size[0], Tile_Size[0]]).copy()
 
             # Render letter
-            letter_s = self.fonts[Scrabble_Font].render(
+            letter_s = self.fonts[Scrabble_Tile_Font].render(
                                 chr(letter),
                                 True,
                                 colors.BLACK)
@@ -86,7 +91,7 @@ class ResourceManager:
             # Render score on the letter
             num_s = self.fonts[Scrabble_Number_Font].render(
                                 str(wh.POINTS[chr(letter)]),
-                                False,
+                                True,
                                 colors.BLACK)
             # Position score on the bottom right of the tile
             # Adds 2px of padding on each side
@@ -99,11 +104,27 @@ class ResourceManager:
 
     def init_fonts(self):
         '''
-        Load all the fonts into memory.
+        Loads all the fonts into memory.
         '''
         for key, fn, sizes in Fonts_Config:
             for size in sizes:
                 self.fonts[key + str(size)] = pygame.font.Font('./res/fonts/' + fn, size)
 
     def init_board_tiles(self):
-        pass
+        '''
+        Loads all the board tiles into memory.
+        '''
+        for key, render, rect in Board_Tiles:
+            t = self.tilesMap.subsurface(*rect).copy()
+
+            if render:
+                # Render bonuses
+                letter_s = self.fonts[Scrabble_Board_Font].render(
+                                        key,
+                                        True,
+                                        colors.BLACK)
+                letter_sx = (Tile_Size[0] - letter_s.get_width())  / 2
+                letter_sy = (Tile_Size[1] - letter_s.get_height()) / 2
+                t.blit(letter_s, (letter_sx, letter_sy))
+
+            self.tiles[key] = t
