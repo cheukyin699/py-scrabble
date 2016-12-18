23c57539523f4889e4c1d773cae15f63ce0277b5
Cheuk Yin Ng <cheukyin699@yahoo.com>
2016-12-17 20:16:18 -0800

    [res] Adds tiles that belong to the board

--- resman.py
+++ resman.py
@@ -5,6 +5,35 @@ import word_helpers as wh
 Scrabble_Number_Font = "Futura9"
 Scrabble_Font = "Futura40"
 Tile_Size = (50, 50)
+'''
+Helps with loading the different fonts and their corresponding sizes.
+Format:
+    (base_name_of_font,
+     font_file_name,
+     list_of_font_sizes)
+'''
+Fonts_Config = [
+    # Used for scrabble tiles
+    ('Futura',      'FuturaExtended.ttf',       [9, 40]),
+    # Used for everything else
+    ('OpenSans',    'OpenSans-Regular.ttf',     [50])
+]
+'''
+Helps with loading the different types of board tiles (i.e. where they are on
+the tile map).
+Format:
+    (tile_shortname,
+     (x, y, w, h))
+'''
+Board_Tiles = [
+    ('NA', ( 50,  0, 50, 50)),     # Normal tile
+    ('MD', (100,  0, 50, 50)),     # That tile in the middle
+    ('DL', (150,  0, 50, 50)),     # Double letter
+    ('DW', (200,  0, 50, 50)),     # Double word
+    ('TL', (250,  0, 50, 50)),     # Triple letter
+    ('TW', (300,  0, 50, 50))      # Triple word
+]
+
 
 class ResourceManager:
     '''
@@ -22,7 +51,7 @@ class ResourceManager:
     def init_tiles(self, fn):
         '''
         Initializes self.tiles dictionary for fast lookup of
-        tile sprite.
+        tile surfaces.
         '''
         # Load tiles
         self.tilesMap = pygame.image.load(fn)
@@ -30,6 +59,13 @@ class ResourceManager:
         # Load fonts (for font writing)
         self.init_fonts()
 
+        # Loads playable tiles
+        self.init_letter_tiles()
+
+        # Loads tiles on the board
+        self.init_board_tiles()
+
+    def init_letter_tiles(self):
         # Initialize tiles by creating them on the fly
         self.tiles = {}
         for letter in range(ord('A'), ord('Z') + 1):
@@ -65,14 +101,9 @@ class ResourceManager:
         '''
         Load all the fonts into memory.
         '''
-        # Lookup table for easy adding/removing
-        font_fns = [
-            # Used for scrabble tiles
-            ('Futura',      'FuturaExtended.ttf',       [9, 40]),
-            # Used for everything else
-            ('OpenSans',    'OpenSans-Regular.ttf',     [50])
-        ]
-
-        for key, fn, sizes in font_fns:
+        for key, fn, sizes in Fonts_Config:
             for size in sizes:
                 self.fonts[key + str(size)] = pygame.font.Font('./res/fonts/' + fn, size)
+
+    def init_board_tiles(self):
+        pass
