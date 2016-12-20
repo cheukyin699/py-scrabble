d92e386daa63a1677aa1885695a9c7464ae80ead
Cheuk Yin Ng <cheukyin699@yahoo.com>
2016-12-19 20:44:21 -0800

    [resman] Adds blank tile
    Almost forgot.

--- resman.py
+++ resman.py
@@ -102,6 +102,11 @@ class ResourceManager:
             t.blit(num_s, (num_sx, num_sy))
             self.tiles[chr(letter)] = t
 
+        # Adds the blank tile (represented by question mark `?')
+        t = self.tilesMap.subsurface(
+                [0, 0, Tile_Size[0], Tile_Size[1]).copy()
+        self.tiles['?'] = t
+
     def init_fonts(self):
         '''
         Loads all the fonts into memory.
