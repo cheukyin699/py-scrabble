dbf3d93b090025905f39671b93d8011deb130378
Cheuk Yin Ng <cheukyin699@yahoo.com>
2016-12-20 21:51:01 -0800

    [deck] Adds basic functionality

--- deck.py
+++ deck.py
@@ -1,12 +1,39 @@
 import tile
 import random
+import word_helpers
 
 class Deck:
+    '''
+    An entire class dedicated to the operations surrounding the Scrabble deck.
+    On initialization, fills in self.d, and shuffles it.
+    '''
     def __init__(self):
-        pass
+        self.init_deck()
 
-    def draw(self, n):
-        pass
+    def init_deck(self):
+        self.d = []
+        for letter, freq in word_helpers.DISTRIBUTION.items():
+            # Blows up letter frequency into long repeated string, then converts
+            # into list and uses said list to extend the dictionary. Repeat for
+            # all the letters, and you get a nice repetitive deck
+            self.d.extend(list(letter * freq))
+
+        # Shuffle it
+        random.shuffle(self.d)
+
+    def take(self, n):
+        '''
+        Tries to get the first n letters of the deck. Shuffles deck afterwards.
+        '''
+        ret = self.d[:n]
+        del self.d[:n]
+        random.shuffle(self.d)
+
+        return ret
 
     def place(self, l):
-        pass
+        '''
+        Appends all tiles from list l to end of deck. Shuffles deck afterwards.
+        '''
+        self.d.extend(l)
+        random.shuffle(self.d)
